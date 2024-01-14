a7fe0877b6a62fcc865a7a038b5babdae624276e
Nataliya Trushina <nitrushina@hotmail.com>
2024-01-14 12:32:20 +0100

    Current directory as working directorycolumn treatment and

--- 01a_merge_multiple_tables_track_analysis.py
+++ 01a_merge_multiple_tables_track_analysis.py
@@ -1,35 +1,45 @@
-import time
-start = time.time()
-
-import numpy as np
 import pandas as pd
 import glob
-pd.set_option('display.expand_frame_repr', False)
-import re
 import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
 
-extension = 'xls'
-all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-df_list = [pd.read_excel(f,sep=";", skiprows=1) for f in all_filenames]
+# Get the directory of the current file and set it as the working directory
+dir_path = os.path.dirname(os.path.realpath(__file__)) 
+
+# Change the current working directory to the script directory
+os.chdir(dir_path)
+
+# Collect all filenames in the working directory that have an extension of .xls or .xlsx
+all_filenames = [i for i in glob.glob('*.xls*')] 
+
+# Read each excel file into a pandas DataFrame and store them in a list
+# Skip the first row of each file assuming it's not part of the header
+df_list = [pd.read_excel(f, skiprows=1) for f in all_filenames] 
+
+# Start with the first DataFrame in the list
 df = df_list[0]
-#https://stackoverflow.com/questions/23668427/pandas-three-way-joining-multiple-dataframes-on-columns
+
+# Define a list of regex patterns for column names to be dropped
+columns_to_drop = ['Collection', 'ReferenceFrame', 'Unit']
+
+# Drop the unwanted columns from the first dataframe
+for pattern in columns_to_drop:
+    df.drop(list(df.filter(regex = pattern)), axis = 1, inplace = True)
+
+# Iterate over the rest of the DataFrames in the list
 for df_ in df_list[1:]:
-    df = df.merge(df_, on=['Original ID','Category','ID','Surpass Object','Original Component ID','Original Component Name','Surpass Object','Surpass Object Data ID','Original Image Name','Original Image ID'])
+    # Drop the unwanted columns from the current DataFrame
+    for pattern in columns_to_drop:
+        df_.drop(list(df_.filter(regex = pattern)), axis = 1, inplace = True)
 
-df.drop(list(df.filter(regex = 'Collection')), axis = 1, inplace = True)
-df.drop(list(df.filter(regex = 'ReferenceFrame')), axis = 1, inplace = True)
-df.drop(list(df.filter(regex = 'Unit')), axis = 1, inplace = True)
+    # Merge the current DataFrame with the main DataFrame
+    # This is done based on a common set of column names
+    df = df.merge(df_, on=['Original ID','Category','ID','Surpass Object','Original Component ID','Original Component Name','Surpass Object Data ID','Original Image Name','Original Image ID'])
 
+# Sort the columns of the DataFrame in alphabetical order
 df = df.reindex(sorted(df.columns), axis=1)
+
+# Sort the DataFrame based on the 'ID' column in ascending order
 df=df.sort_values(by=["ID"], ascending=True)
-print(df)
 
-#another way to choose columns of interest
-#df=df[['ID','Original ID','Track Displacement Length Reference Frame']]
-
+# Export the DataFrame to a CSV file
 df.to_csv("merged_table.csv", sep=';', index=False, header=True)
-
-end = time.time()
-print(end - start)
-
